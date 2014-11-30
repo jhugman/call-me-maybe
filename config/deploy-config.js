968352ee40fcc62961e44c58aa282c47679b50d6
Fabien O'Carroll <fabien@allou.is>
2014-11-30 20:41:23 +0000

    add default session-hosts for prod

--- config/deploy-config.js
+++ config/deploy-config.js
@@ -16,6 +16,9 @@ module.exports = {
     'opentok-session': '2_MX40NTA5MDg2Mn5-MTQxNzExMTc3NDczN345QTg2bUxPRy9Xb0ptdjUveHBQR3F2Q0d-fg',
     'opentok-token-caller': 'T1==cGFydG5lcl9pZD00NTA5MDg2MiZzaWc9ODJmZTZiNGQwNzQ3NmJiY2U3OTkwNWFlMjE1ZmIyYTNhNzg3NDMzYzpyb2xlPXB1Ymxpc2hlciZzZXNzaW9uX2lkPTFfTVg0ME5UQTVNRGcyTW41LU1UUXhOek0yTVRjek1EWXpPWDVXYUcxM2RXNWpWMmxEZEhkMldXdHVORmxzZVM4d2MxbC1mZyZjcmVhdGVfdGltZT0xNDE3MzYxNzkyJm5vbmNlPTAuNDMzNzc3NzMwMDIyMjM5NiZleHBpcmVfdGltZT0xNDE5OTUzNzIw',
     'opentok-token-callee': 'T1==cGFydG5lcl9pZD00NTA5MDg2MiZzaWc9YjlkNmNhNjQyZTZjMzVmMzFlNjczMGYxMjA5NDhkOGJiOTcyZjE0Nzpyb2xlPXB1Ymxpc2hlciZzZXNzaW9uX2lkPTFfTVg0ME5UQTVNRGcyTW41LU1UUXhOek0yTVRjek1EWXpPWDVXYUcxM2RXNWpWMmxEZEhkMldXdHVORmxzZVM4d2MxbC1mZyZjcmVhdGVfdGltZT0xNDE3MzYxODMxJm5vbmNlPTAuNTU1NzQzNjA2NzMwODEyNSZleHBpcmVfdGltZT0xNDE5OTUzNzIw',
+    'session-host': {
+      '': 'moz.allou.is'
+    }
   },
 
   hello: {
@@ -23,6 +26,9 @@ module.exports = {
     'opentok-session': '2_MX40NTA5MDg2Mn5-MTQxNzExMTc3NDczN345QTg2bUxPRy9Xb0ptdjUveHBQR3F2Q0d-fg',
     'opentok-token-caller': 'T1==cGFydG5lcl9pZD00NTA5MDg2MiZzaWc9ODJmZTZiNGQwNzQ3NmJiY2U3OTkwNWFlMjE1ZmIyYTNhNzg3NDMzYzpyb2xlPXB1Ymxpc2hlciZzZXNzaW9uX2lkPTFfTVg0ME5UQTVNRGcyTW41LU1UUXhOek0yTVRjek1EWXpPWDVXYUcxM2RXNWpWMmxEZEhkMldXdHVORmxzZVM4d2MxbC1mZyZjcmVhdGVfdGltZT0xNDE3MzYxNzkyJm5vbmNlPTAuNDMzNzc3NzMwMDIyMjM5NiZleHBpcmVfdGltZT0xNDE5OTUzNzIw',
     'opentok-token-callee': 'T1==cGFydG5lcl9pZD00NTA5MDg2MiZzaWc9YjlkNmNhNjQyZTZjMzVmMzFlNjczMGYxMjA5NDhkOGJiOTcyZjE0Nzpyb2xlPXB1Ymxpc2hlciZzZXNzaW9uX2lkPTFfTVg0ME5UQTVNRGcyTW41LU1UUXhOek0yTVRjek1EWXpPWDVXYUcxM2RXNWpWMmxEZEhkMldXdHVORmxzZVM4d2MxbC1mZyZjcmVhdGVfdGltZT0xNDE3MzYxODMxJm5vbmNlPTAuNTU1NzQzNjA2NzMwODEyNSZleHBpcmVfdGltZT0xNDE5OTUzNzIw',
+    'session-host': {
+      '': 'hello.allou.is'
+    }
   },
 
 
